92a01b2e569421bbfead6650963dbf9624f297f3
Deanna Leon <2019073@icacademy.org>
2017-11-18 22:54:59 +0000

    Add HTML and CSS from front end mockup to application

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,5 +4,16 @@ class TodoController < ApplicationController
     def show
         @todo_description="Make a Curriculum"
         @todo_pomodoro_estimate=4
+        todo_id=params[:id]
+        
+        if todo_id=='1'
+            @todo_description="Make a Curriculum"
+            @todo_pomodoro_estimate=4
+        
+        elsif 
+            @todo_id=='2'
+            @todo_description="Buy Workshop Supplies"
+            @todo_pomodoro_estimate=3
+        end
     end
 end
